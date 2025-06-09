e880c439deee9ead7566725e815ccca4c78114c8
matias-gonz <maigonzalez@fi.uba.ar>
2025-06-09 12:31:33 +0200

    Add Hero component and integrate it into LandingPage

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,17 @@
+export const Hero = () => {
+  return (
+    <div className="flex h-screen w-screen bg-[url(/src/assets/landing_bg.png)] bg-cover bg-center items-end">
+      <div className="flex flex-col px-30 py-8 gap-4">
+        <div className="text-6xl leading-18 max-w-3xl">
+          Lorem ipsum dolor sit amet consectetur adipiscing.
+        </div>
+        <div className="flex items-center h-8 gap-2">
+          <div className="bg-amber-300 h-full aspect-square rounded-full flex items-center justify-center font-bold">
+            &gt;
+          </div>
+          <div className="text-lg font-bold">Get Started</div>
+        </div>
+      </div>
+    </div>
+  );
+}; 
